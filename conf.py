9d4ac042ef053c9897747bd632c1b1962f35d14b
notify <notify-ctrl@qq.com>
2024-02-25 03:49:45 +0800

    add favicon

--- conf.py
+++ conf.py
@@ -76,6 +76,7 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = 'favicon.ico'
 
 # PDF option
 latex_engine = 'xelatex'
